2b1d2940a28fce1cc3944e0f1bf422e1cd793eb3
Hugo4IT <hugo.vandekuilen1234567890@gmail.com>
2024-04-27 13:18:58 +0200

    sadf

--- hug_compiler/src/lib.rs
+++ hug_compiler/src/lib.rs
@@ -1,1 +1,21 @@
-pub fn compile(program: String) {}
+#[derive(Debug, Clone)]
+pub struct SymbolRef {
+    pub mangled_name: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct Symbol {
+    pub mangled_name: String,
+    pub name: String,
+    pub kind: SymbolKind,
+}
+
+#[derive(Debug, Clone)]
+pub enum SymbolKind {
+    Class,
+    Function,
+    Variable,
+}
+
+#[derive(Debug)]
+pub struct Compiler {}
